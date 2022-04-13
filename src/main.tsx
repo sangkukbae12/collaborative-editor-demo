f51885093e9f81a3d748c7ccca363d6a522b0253
ben-redbrick <ben@redbrick.space>
2022-04-13 20:39:25 +0900

    add recoil atom and debug observer

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import App from './App';
+import DebugObserver from './store/debug';
+import './worker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RecoilRoot>
+      <DebugObserver />
+      <App />
+    </RecoilRoot>
+  </React.StrictMode >,
   document.getElementById('root')
-)
+);
