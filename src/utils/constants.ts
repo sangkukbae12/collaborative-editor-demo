a0799de486fa1b030878edf382f92ba8ff11a69d
ben-redbrick <ben@redbrick.space>
2022-04-13 20:46:21 +0900

    add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const EDITOR_OPTIONS = {
+  VALUE: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+  THEME: 'vs-dark',
+  LANGUAGE: 'javascript',
+  FONT_SIZE: 18
+};
+
+export const WEBSOCKET = {
+  URL: 'ws://54.180.91.17:1234',
+  ROOM: 'monaco-demo'
+};
